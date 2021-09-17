ff2378cf6b10fe78754ed10ad8f115655b8fdfcc
Juan Camilo <juancmz98@gmail.com>
2021-09-17 01:38:39 -0500

    Searches model created

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 const {
   inquiererMenu,
-  pause
-} = require("./helpers/inquirer")
+  pause,
+  readInput
+} = require("./helpers/inquirer");
+const Searchs = require("./models/searchs");
 
 const main = async() => {
+  const searches = new Searchs()
   let opt
 
   do {
     opt = await inquiererMenu()
-    console.log({opt})
+    
+    switch (opt) {
+      case 1:
+        // Show message
+        const place = await readInput('Ciudad: ')
+        console.log(place)
+
+        // Search places
+
+        // Pick a place
+
+        // Weather
+
+        //Show results
+        console.log('\nInformación de la ciudad\n'.green)
+        console.log('Ciudad:', )
+        console.log('Lat:', )
+        console.log('Lng:', )
+        console.log('Temperatura:', )
+        console.log('Mínima:', )
+        console.log('Máxima:', )
+        break;
+    
+      default:
+        break;
+    }
     
     if (opt !== 0) await pause()
   } while (opt !== 0);
---
+++ models/searchs.js
@@ -0,0 +1,14 @@
+class Searchs {
+  constructor() {
+    //TODO: Read DB if exist
+  }
+
+  async city(place = '') {
+    //TODO Petición HTTP
+    console.log(place)
+
+    return [] // Return places that matched with the parameter
+  }
+}
+
+module.exports = Searchs
